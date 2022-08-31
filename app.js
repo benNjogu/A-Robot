cf1206d6702e45ab04646ac0efc38a2efcf55576
benNjogu <kamandemachafi@gmail.com>
2022-08-31 13:56:14 +0300

    Moving the robot

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function buildGraph(edges) {
   let graph = Object.create(null); //Creates an object with no prototype.
   function addEdge(from, to) {
     if (graph[from] == null) {
-      graph[from] == [to];
+      graph[from] = [to];
     } else {
       graph[from].push(to);
     }
@@ -43,3 +43,34 @@ function buildGraph(edges) {
 
 const roadGraph = buildGraph(roads);
 console.log(roadGraph);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" },
+]);
+let next = first.move("Alice's House");
+
+console.log(next.place); //-> Alice's House
+console.log(next.parcels); //-> []
+console.log(first.place);//-> Post Office
