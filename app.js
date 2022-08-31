43c90fa2bd7252a82eebb5456a66544cab3b012d
benNjogu <kamandemachafi@gmail.com>
2022-08-31 12:03:46 +0300

    The roads

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-alert("hello world");
+/**
+ * An automation.
+ * a little program that perfoms a task in a virtual world.
+ * a mail-delivery robot picking up and dropping off parcels.
+ */
+
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+//The n/w of roads in Meadowfield form a graph.
+//A graph is a collection of points(places in the village) with lines between them(roads).
